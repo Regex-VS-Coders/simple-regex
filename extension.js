1e87deb413ee21544b26b762ec86dc2d6fb304ec
Kris Stern <krisstern@outlook.com>
2024-05-04 17:01:18 +0800

    fix: update config in launch.json and fix comment in extension.js

--- extension.js
+++ extension.js
@@ -15,7 +15,7 @@ function activate(context) {
 	console.log('Congratulations, your extension "simple-regex" is now active!');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
+	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('simple-regex.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
